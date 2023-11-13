e79efa2230bbf9b3aa06ccf5cbd0a083eb67b587
Viktor Andersen <vikfand@gmail.com>
2023-11-13 13:14:56 +0100

    Disable gist bcz prerender error

--- components/Gist.server.vue
+++ components/Gist.server.vue
@@ -1,13 +1,18 @@
 <template>
-  <div v-if="!!gistData">
-    <div v-html="gistData.div"></div>
+  <div>
+    <a :href="`https://gist.github.com/${props.gist}`">
+      https://gist.github.com/{{ gist }}
+    </a>
   </div>
+  <!--<div v-if="!!gistData">
+    <div v-html="gistData.div"></div>
+  </div>-->
 </template>
 
 <script setup lang="ts">
 const props = defineProps({ gist: { type: String, required: true } });
 
-const { data: gistData } = await useFetch(
+/*const { data: gistData } = await useFetch(
   `https://gist.github.com/${props.gist}.json`
-);
+);*/
 </script>
